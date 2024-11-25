234866c839b6358f6c9d32225e1c575725395507
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2024-11-25 09:19:28 +0100

    Ejercicio 31 completado

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,24 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+
+        String ruta = "src/main/resources/lorem.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(ruta))){
+            String linea = "";
+            while ((linea = br.readLine()) != null){
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
